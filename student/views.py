d9fa00027ba9c3b9dcea6c95918d23dd9f0a18a7
kacfal <kacfal181195@gmail.com>
2019-01-05 16:51:47 +0100

    Add views for student

--- student/views.py
+++ student/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Student
+from .form import StudentModelForm
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    ListView,
+    UpdateView,
+    DetailView
+)
 
-# Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+
+
+class StudentCreateView(CreateView):
+    template_name = 'student_create.html'
+    form_class = StudentModelForm
+    queryset = Student.objects.all()
+
+    def get_success_url(self):
+        return '/student'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+
+class StudentDeleteView(DeleteView):
+    template_name = 'student_delete.html'
+
+    def get_object(self, queryset=None):
+        _id = self.kwargs.get("id")
+        return get_object_or_404(Student, id=_id)
+
+    def get_success_url(self):
+        return reverse('student:student-list')
+
+
+class StudentListView(ListView):
+    template_name = 'student_details.html'
+    queryset = Student.objects.all()
+
+
+class StudentUpdateView(UpdateView):
+    template_name = 'student_create.html'
+    form_class = StudentModelForm
+    # queryset = Article.objects.all()
+
+    def get_object(self, queryset=None):
+        _id = self.kwargs.get("id")
+        return get_object_or_404(Student, id=_id)
+
+    def get_success_url(self):
+        return '/student'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+
+class StudentDetailView(DetailView):
+    template_name = 'student_details.html'
+
+    def get_object(self, queryset=None):
+        _id = self.kwargs.get('id')
+        return get_object_or_404(Student, id=_id)
+
+
