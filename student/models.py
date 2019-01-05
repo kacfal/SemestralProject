dfc9c0ef777c91c6275b176299f8d718f9de201f
kacfal <kacfal181195@gmail.com>
2019-01-05 14:36:06 +0100

    Create model student

--- student/models.py
+++ student/models.py
@@ -4,9 +4,13 @@ from django.db import models
 
 
 class Student(models.Model):
+    STATE_OF_STUDDING = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive')
+    )
     name = models.CharField(blank=True, max_length=120)
     last_name = models.CharField(blank=True, max_length=120)
     index = models.CharField(blank=True, max_length=6)
     profile = models.CharField(blank=True, max_length=120)
-    status = models.BooleanField(default=True)
+    status = models.CharField(max_length=10, choices=STATE_OF_STUDDING)
 
