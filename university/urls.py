115ea72e1032cf15ac50ea4dad0a92671313c232
kacfal <kacfal181195@gmail.com>
2019-01-20 19:18:11 +0100

    Add details view

--- university/urls.py
+++ university/urls.py
@@ -2,15 +2,17 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import (
     UniversityDeleteAPIView,
-    UniversityListCreateAPIView
+    UniversityListAPIView,
+    UniversityDetailsAPIView
 
 )
 
 
 app_name = 'university'
 urlpatterns = [
-    path('university/', UniversityListCreateAPIView.as_view(), name='university-api-list-create'),
-    path('university/<int:university_id>/delete', UniversityDeleteAPIView.as_view(), name='university-api-delete')
+    path('', UniversityListAPIView.as_view(), name='api-list'),
+    path('delete/<int:id>/', UniversityDeleteAPIView.as_view(), name='api-delete'),
+    path('details/<int:id>/', UniversityDetailsAPIView.as_view(), name='api-details')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- university/views.py
+++ university/views.py
@@ -1,21 +1,20 @@
-from rest_framework.generics import ListCreateAPIView, DestroyAPIView
-
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView, RetrieveAPIView
 from .models import University
 from .serializers import UniversitySerializer
 
 
-class UniversityListCreateAPIView(ListCreateAPIView):
+class UniversityListAPIView(ListCreateAPIView):
+    queryset = University.objects.all()
     serializer_class = UniversitySerializer
-
-    def perform_create(self, serializer):
-        serializer.save()
-
-    def get_queryset(self):
-        _id = self.kwargs.get("id")
-        return University.objects.all()
 
 
 class UniversityDeleteAPIView(DestroyAPIView):
     serializer_class = UniversitySerializer
     queryset = University.objects.all()
-    lookup_field = 'university_id'
+    lookup_field = 'id'
+
+
+class UniversityDetailsAPIView(RetrieveAPIView):
+    serializer_class = UniversitySerializer
+    queryset = University.objects.all()
+    lookup_field = 'id'
