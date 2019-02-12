175f46e2e7229ea3d9eb4ed89dce050ac6f6c2a5
kacfal <kacfal181195@gmail.com>
2019-02-12 19:26:32 +0100

    Fix repository

--- Server/university/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import University
-
-# Register your models here.
-admin.site.register(University)
-
-
--- Server/university/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import University
-
-
-class UniversityModelForm(forms.ModelForm):
-    class Meta:
-        model = University
-        fields = '__all__'
