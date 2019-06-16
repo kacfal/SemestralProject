1a5de64cd7e7cfc7c4b9bf063a43d21d40e8dcbd
kacfal <kacfal181195@gmail.com>
2019-06-16 22:50:04 +0200

    Update student model

--- student/models.py
+++ student/models.py
@@ -4,17 +4,12 @@ from university.models import University
 
 
 class Student(models.Model):
-    STATE_OF_STUDDING = (
-        ('Active', 'Active'),
-        ('Inactive', 'Inactive')
-    )
     university_id = models.ForeignKey(University, on_delete=models.CASCADE, related_name='universities')
     department = models.CharField(max_length=120)
     profile = models.CharField(max_length=120)
     name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     index = models.DecimalField(max_digits=999999, decimal_places=0, unique=True)
-    status = models.TextField(choices=STATE_OF_STUDDING)
     deficit = models.IntegerField(default=0, blank=True)
 
     def get_absolute_url(self):
